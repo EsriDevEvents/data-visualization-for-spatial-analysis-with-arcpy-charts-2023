0fae96a9549dae1b38914b0d65f55be1f058bc36
Chris Allen <98368680+chartshark@users.noreply.github.com>
2023-03-06 13:00:50 -0800

    Update chart_script.py

--- code/chart_script.py
+++ code/chart_script.py
@@ -4,7 +4,7 @@ showing 2020 election vote breakdowns for each state.
 
 import arcpy
 
-lyr = arcpy.mp.ArcGISProject('current').activeMap.listTables('election2020')[0]
+lyr = arcpy.mp.ArcGISProject('current').activeMap.listLayers('votes2020')[0]
 # get list of unique state names
 state_names = set([row[0] for row in arcpy.da.SearchCursor(lyr, 'state')])
 
@@ -13,10 +13,10 @@ for state in state_names:
     # set definition query to only show current state
     lyr.definitionQuery = f"state = '{state}'"
     # create pie chart showing vote proportions
-    pie = arcpy.charts.Pie(numberFields=['REPUBLICAN', 'DEMOCRAT', 'OTHER'], 
+    pie = arcpy.charts.Pie(numberFields=['gop', 'dem', 'other'], 
                            title=state, groupingPercent=0, displaySize=(700, 700), 
                            dataSource=lyr)
     # export as svg
-    pie.exportToSVG(f'c:/temp/{state}.svg')
+    pie.exportToSVG(f'{state}.svg')
     # reset definition query
     lyr.definitionQuery = None
